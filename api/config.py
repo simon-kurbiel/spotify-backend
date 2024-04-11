fcdc60815717e69430653b47cda3b19379919e85
simon-kurbiel <simonkurbiel08@gmail.com>
2024-04-11 07:14:49 -0400

    added variables

---
+++ api/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+import os
+
+
+
+class Settings(BaseSettings):
+    SECRET_KEY:str= os.environ.get("SECRET_KEY")
+    REDIRECT_URI:str=os.environ.get("CLIENT_ID")
+    CLIENT_ID:str=os.environ.get("REDIRECT_URI")
+    CLIENT_SECRET:str=os.environ.get("CLIENT_SECRET")
+    SPOTIFY_AUTH_URL:str = "https://accounts.spotify.com/authorize"
+    STATE_VALUE:str =os.environ.get("STATE_VALUE")
+        
+settings = Settings()
